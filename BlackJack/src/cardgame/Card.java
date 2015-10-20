fc937ed9aca2cea7dc7a0567b8610b70ed1a12b5
Sjoelander <anton.sjoelander@gmail.com>
2015-10-20 18:47:15 +0200

    Added toString

---
+++ BlackJack/src/cardgame/Card.java
@@ -0,0 +1,44 @@
+package cardgame;
+public class Card {
+	private String suit;    
+	private int value;    
+
+	public Card(String suit, int value) {
+		this.suit = suit;
+		this.value = value;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public int getValue() {
+		return value > 10 ? 10 : value;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(suit);
+		sb.append(" ");
+		
+		switch(value) {
+		case 1: 
+			sb.append("Ace");
+			break;
+		case 11: 
+			sb.append("Jack");
+			break;
+		case 12: 
+			sb.append("Queen");
+			break;
+		case 13: 
+			sb.append("King");
+			break;
+		default: 
+			sb.append(value);
+			break;
+		}
+		return sb.toString();
+	}
+}
