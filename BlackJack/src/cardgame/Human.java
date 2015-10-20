ed29888bfc4858b6480ad87977d702537c85fb5c
Sjoelander <anton.sjoelander@gmail.com>
2015-10-20 18:48:55 +0200

    More input

---
+++ BlackJack/src/cardgame/Human.java
@@ -0,0 +1,36 @@
+package cardgame;
+import java.util.Scanner;
+public class Human extends Player {
+	Scanner scan;
+	public Human(String name) {
+		super(name);
+		scan = new Scanner(System.in);
+	}
+
+	/**
+	 * Frågar spelaren efter vad hen vill göra och åberopar metoden hit / stand 
+	 * beroende på svar
+	 * @param deck Deck med kort
+	 * @param bl För rules
+	 */
+	public void whatDo (Deck deck, BlackJack bj) {
+		while(true) {
+			System.out.println("Choose what to do. 'Hit or Stand? (H/S)" + "\n Type 'rules' for rules");
+			String line = scan.nextLine().toLowerCase();
+			if(line.equals("h")) {
+				hit(deck);
+				break;
+			}
+			else if(line.equals("s")) {
+				stand();
+				break;
+			} else if (line.equals("rules")) {
+				// TODO bl.printRules();
+			} else if (line.equals("sdoublepalsikt sokd")) {
+				// TODO IF DOUBLE/SPLIT == TRUE => OPTION FOR DOUBLE/SPLIT
+			} else{
+				System.out.println("Not a correct input");
+			}
+		}
+	}
+}
